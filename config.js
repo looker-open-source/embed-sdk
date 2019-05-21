84351f6a2bd1897d47107bf4a4bfd52273942c05
Will Scullin <scullin@looker.com>
2019-05-21 14:37:42 -0700

    Proper default host.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  host: process.env.LOOKER_EMBED_HOST || 'self-signed.localhost.com:9999',
+  host: process.env.LOOKER_EMBED_HOST || 'self-signed.looker.com:9999',
   secret: process.env.LOOKER_EMBED_SECRET || 'ranger2'
 }
