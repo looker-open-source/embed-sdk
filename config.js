c7046d379f1eb155f8aab89d63199ca7d7653456
Danny Thorpe <danny@looker.com>
2019-11-03 12:12:21 -0800

    Make nodejs demo web app server use settings from config.js Added env.LOOKER_DEMO_HOST and env.LOOKER_DEMO_PORT to config.js use config.demo_host and config.demo_port in webpack dev server config options.

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   host: process.env.LOOKER_EMBED_HOST || 'self-signed.looker.com:9999',
-  secret: process.env.LOOKER_EMBED_SECRET || 'ranger2'
+  secret: process.env.LOOKER_EMBED_SECRET,
+  demo_host: process.env.LOOKER_DEMO_HOST,
+  demo_port: process.env.LOOKER_DEMO_PORT,
 }
