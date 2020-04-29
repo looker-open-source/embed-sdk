97b42e23700b57a127a42e49487f75c2b684b39c
Chris Seymour <chris.seymour@looker.com>
2020-04-29 16:59:04 -0700

    Removing port number
    Adding to https://github.com/looker-open-source/embed-sdk/pull/39

--- demo/demo_config.ts
+++ demo/demo_config.ts
@@ -1,5 +1,5 @@
-// The address of your Looker instance. Required.
-export const lookerHost = 'self-signed.looker.com:9999'
+// The address of your Looker instance. Required.  (Port must be included if it is required to reach the Looker server from browser clients, e.g. self-signed.looker.com:9999) 
+export const lookerHost = 'self-signed.looker.com'
 
 // A dashboard that the user can see. Set to 0 to disable dashboard.
 export const dashboardId = 1
