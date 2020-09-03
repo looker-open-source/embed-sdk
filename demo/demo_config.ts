b5bd7819f97144894d5d1e7a8edd2eca0656daf2
Will Scullin <scullin@looker.com>
2020-09-03 17:36:30 +0000

    undo local edit

--- demo/demo_config.ts
+++ demo/demo_config.ts
@@ -9,10 +9,10 @@ export const lookerHost = 'self-signed.looker.com:9999'
 export const dashboardId = 1
 
 // A Look that the user can see. Set to 0 to disable look demo.
-export const lookId = 0
+export const lookId = 1
 
 // An Explore that the user can see. Set to '' to disable explore demo.
-export const exploreId = ''
+export const exploreId = 'thelook::orders'
 
 // An Extension that the user can see. Set to '' to disable extension demo.
 // export const extensionId = 'extension::my-great-extension'
