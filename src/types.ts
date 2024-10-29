a4c0ddba642e0921db1f419a4021fe936429eadc
Eric <eric@greenspacehealth.com>
2024-10-28 18:56:58 -0700

    Add DrillModalDownloadEvent

--- src/types.ts
+++ src/types.ts
@@ -357,6 +357,21 @@ export interface DrillMenuEvent extends LookerEmbedEvent {
   addFilterJson: AddFilterJson
 }
 
+ /**
+  * Drill modal download event
+  */
+ export interface DrillModalDownloadEvent extends LookerEmbedEvent {
+  dashboard: {
+    id: string | number
+    title: string
+    url: string
+    absoluteUrl: string
+    dashboard_filters: LookerEmbedFilterParams
+  }
+  drillExploreUrl: string
+  fileFormat: string
+}
+
 /**
  * Drill Modal Explore from Here event
  *
@@ -563,6 +578,10 @@ export interface LookerEmbedEventMap {
     this: LookerEmbedBase,
     event: DrillMenuEvent
   ) => CancellableEventResponse | undefined
+  'drillmodal:download': (
+    this: LookerEmbedBase,
+    event: DrillModalDownloadEvent
+  ) => void
   'drillmodal:explore': (
     this: LookerEmbedBase,
     event: DrillModalExploreEvent
